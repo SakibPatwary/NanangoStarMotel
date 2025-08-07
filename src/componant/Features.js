1c28842d5ef0ab2d5345496fc6f10fff0a27edd2
Sakib Patwary <shakibpatwary001@gmail.com>
2025-08-08 04:06:52 +0600

    FAQ updated

--- src/componant/Features.js
+++ src/componant/Features.js
@@ -16,20 +16,20 @@ const Features = () => {
                     <motion.div
                         whileHover={{ scale: 1.02 }}
                     >
-                    <Card className="py-4 px-2 text-center" style={{ minHeight: '180px', backgroundColor:'#d1cfcf'}}>
+                        <Card className="py-4 px-2 text-center" style={{ minHeight: '180px', backgroundColor: '#d1cfcf' }}>
                             <Card.Title style={{ fontWeight: 'bold' }}>Accommodation</Card.Title>
-                            <Card.Text style={{textAlign:'justify'}}>
+                            <Card.Text style={{ textAlign: 'justify' }}>
                                 The Nanango Star Motel offers a range of accommodation options to suit different guest needs, from standard single rooms to family/twin share rooms.
                             </Card.Text>
                         </Card></motion.div>
                 </Col>
-                <Col xs={12} sm={12}  md={6} lg={4} className='pb-3'>
+                <Col xs={12} sm={12} md={6} lg={4} className='pb-3'>
                     <motion.div
                         whileHover={{ scale: 1.02 }}
                     >
-                    <Card className="py-4 px-2 text-center" style={{ minHeight: '180px', backgroundColor: '#d1cfcf' }}>
+                        <Card className="py-4 px-2 text-center" style={{ minHeight: '180px', backgroundColor: '#d1cfcf' }}>
                             <Card.Title style={{ fontWeight: 'bold' }}>On-site Parking</Card.Title>
-                        <Card.Text style={{ textAlign: 'justify' }}>
+                            <Card.Text style={{ textAlign: 'justify' }}>
                                 The motel provides ample on-site parking space, making it convenient for guests to park their vehicles.
                             </Card.Text>
 
@@ -39,10 +39,11 @@ const Features = () => {
                     <motion.div
                         whileHover={{ scale: 1.02 }}
                     >
-                    <Card className="py-4 px-2 text-center" style={{ minHeight: '180px', backgroundColor: '#d1cfcf' }}>
-                            <Card.Title style={{ fontWeight: 'bold' }}>24 Hours Manager on-site</Card.Title>
-                        <Card.Text style={{ textAlign: 'justify' }}>
-                                The motel has manager on-site 24/7 to assist guest with chech-in and all other services. Reception desk is open from 8AM till 8PM and after hours assistance by calling the motel number.
+                        <Card className="py-4 px-2 text-center" style={{ minHeight: '180px', backgroundColor: '#d1cfcf' }}>
+                            <Card.Title style={{ fontWeight: 'bold' }}>24 Hours Manager Support
+                            </Card.Title>
+                            <Card.Text style={{ textAlign: 'justify' }}>
+                                The motel has manager support 24/7 to assist guest with all the enquiries and services. Reception desk is open from 9AM till 7PM and after hours assistance by calling the motel number.
                             </Card.Text>
                         </Card></motion.div>
                 </Col>
@@ -50,33 +51,33 @@ const Features = () => {
                     <motion.div
                         whileHover={{ scale: 1.02 }}
                     >
-                    <Card className="py-4 px-2 text-center" style={{ minHeight: '180px', backgroundColor: '#d1cfcf' }}>
+                        <Card className="py-4 px-2 text-center" style={{ minHeight: '180px', backgroundColor: '#d1cfcf' }}>
                             <Card.Title style={{ fontWeight: 'bold' }}>Free Wi-Fi</Card.Title>
-                        <Card.Text style={{ textAlign: 'justify' }}>
-                            Offering free Wi-Fi is essential for most travelers, especially corporate guests or those on vacation.
-                        </Card.Text>
+                            <Card.Text style={{ textAlign: 'justify' }}>
+                                Offering free Wi-Fi is essential for most travelers, especially corporate guests or those on vacation.
+                            </Card.Text>
                         </Card></motion.div>
                 </Col>
                 <Col xs={12} sm={12} md={6} lg={4} className='pb-3'>
                     <motion.div
                         whileHover={{ scale: 1.02 }}
                     >
-                    <Card className="py-4 px-2 text-center" style={{ minHeight: '180px', backgroundColor: '#d1cfcf' }}>
+                        <Card className="py-4 px-2 text-center" style={{ minHeight: '180px', backgroundColor: '#d1cfcf' }}>
                             <Card.Title style={{ fontWeight: 'bold' }}>Housekeeping Service</Card.Title>
-                        <Card.Text style={{ textAlign: 'justify' }}>
-                            Regular cleaning and maintenance of guest rooms, including bed linens and towels.
-                        </Card.Text>
+                            <Card.Text style={{ textAlign: 'justify' }}>
+                                Regular cleaning and maintenance of guest rooms, including bed linens and towels.
+                            </Card.Text>
                         </Card></motion.div>
                 </Col>
-                <Col xs={12} sm={12}  md={6} lg={4} className='pb-3'>
+                <Col xs={12} sm={12} md={6} lg={4} className='pb-3'>
                     <motion.div
                         whileHover={{ scale: 1.02 }}
                     >
-                    <Card className="py-4 px-2 text-center" style={{ minHeight: '180px', backgroundColor: '#d1cfcf' }}>
+                        <Card className="py-4 px-2 text-center" style={{ minHeight: '180px', backgroundColor: '#d1cfcf' }}>
                             <Card.Title style={{ fontWeight: 'bold' }}>Tea Coffee and Kitchenette</Card.Title>
-                        <Card.Text style={{ textAlign: 'justify' }}>
-                            Complementary tea, coffee, sugar biscuits provided with each room. Microwave, fridge, kettle and toaster along with glasses, plates and cutleries are some of the inclusions with each room.
-                        </Card.Text>
+                            <Card.Text style={{ textAlign: 'justify' }}>
+                                Complementary tea, coffee, sugar biscuits provided with each room. Microwave, fridge, kettle and toaster along with glasses, plates and cutleries are some of the inclusions with each room.
+                            </Card.Text>
                         </Card></motion.div>
                 </Col>
             </Row>
